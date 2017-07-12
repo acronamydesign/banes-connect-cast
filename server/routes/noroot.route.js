643d382d53cf6a8caf28df2dbd88a3814fe1b60c
root <root@vml-dpl-web8.bathnes.gov.uk>
2017-07-12 15:07:52 +0100

    live => integration, plan to patch feed and create integration copy. feeds error handeling required, skip over data malformed

--- server/routes/noroot.route.js
+++ server/routes/noroot.route.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const url = require("url");
 function disableRoot(app, serverConf) {
-    app.get('/', (req, res) => {
+    app.get('/bootstrap', (req, res) => {
+        const query = url.parse(req.url, true).query;
+        res.cookie("location", query.location);
+        res.redirect("/app");
+    });
+    app.get('/app/bootstrap', (req, res) => {
         const query = url.parse(req.url, true).query;
         res.cookie("location", query.location);
         res.redirect("/app");
--- server/routes/noroot.route.ts
+++ server/routes/noroot.route.ts
@@ -1,8 +1,15 @@
 import * as url from "url";
 
 export function disableRoot( app, serverConf ){
-    app.get('/', (req, res)=>{
+    app.get('/bootstrap', (req, res)=>{
         
+        const query = url.parse(req.url, true).query;
+
+        res.cookie("location", query.location)
+        res.redirect("/app")
+    })
+	app.get('/app/bootstrap', (req, res)=>{
+
         const query = url.parse(req.url, true).query;
 
         res.cookie("location", query.location)
